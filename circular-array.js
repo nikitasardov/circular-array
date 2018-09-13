1cb37c2b7f22fe93b9c6c0a3d3e58a32191a936d
nsardov <nsardov@gmail.com>
2018-09-13 13:43:52 +0400

    add minified js

--- circular-array.js
+++ circular-array.js
@@ -2,51 +2,36 @@ function CircArray(arrLength) {
     let arr = [],
         currIndex = -1;
 
-    let status = function() {
-        console.log({
-            length: arr.length,
-            currentIndex: currIndex,
-            currentValue: arr[currIndex],
-            arr: arr
-        });
-    };
-
-    let setArray = function(newArray) {
-        arr = newArray;
-    };
-
-    let toArray = function() {
-        return arr;
-    };
-
-    let push = function(el) {
-        arr.push(el);
-    };
-
-    let prev = function() {
-        if (0 > (currIndex - 1)) currIndex = arr.length - 1;
-        else currIndex--;
-        return arr[currIndex];
-    };
-
-    let current = function() {
-        return arr[currIndex];
-    };
-
-    let next = function() {
-        if (arr.length <= (currIndex + 1)) currIndex = 0;
-        else currIndex++;
-        return arr[currIndex];
-    };
-
     return {
-        setArray: setArray,
-        toArray: toArray,
-        status: status,
-        push: push,
-
-        prev: prev,
-        curr: current,
-        next: next
+        setArray: function(newArray) {
+            arr = newArray;
+        },
+        toArray: function() {
+            return arr;
+        },
+        status: function() {
+            console.log({
+                length: arr.length,
+                currentIndex: currIndex,
+                currentValue: arr[currIndex],
+                arr: arr
+            });
+        },
+        push: function(el) {
+            arr.push(el);
+        },
+        prev: function() {
+            if (0 > (currIndex - 1)) currIndex = arr.length - 1;
+            else currIndex--;
+            return arr[currIndex];
+        },
+        curr: function() {
+            return arr[currIndex];
+        },
+        next: function() {
+            if (arr.length <= (currIndex + 1)) currIndex = 0;
+            else currIndex++;
+            return arr[currIndex];
+        }
     }
 }
